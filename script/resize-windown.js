1f4893d3e65e260dabcc701f17c52f6ef15c16a1
FlorencioGoncalves <florenciopaul@gmail.com>
2022-10-19 11:27:54 +0100

    Mexendo na responsividade

---
+++ script/resize-windown.js
@@ -0,0 +1,18 @@
+document.querySelector("body").style.height =
+  document.documentElement.clientHeight + "px";
+
+let addEvent = function (elem, type, eventHandle) {
+  if (elem == null || typeof elem == "undefined") return;
+  if (elem.addEventListener) {
+    elem.addEventListener(type, eventHandle, false);
+  } else if (elem.attachEvent) {
+    elem.attachEvent("on" + type, eventHandle);
+  } else {
+    elem["on" + type] = eventHandle;
+  }
+};
+
+addEvent(window, "resize", function () {
+  document.querySelector("body").style.height =
+    document.documentElement.clientHeight + "px";
+});
