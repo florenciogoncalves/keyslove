d7d8ee9d2cb7970b455d296b53d42216c9295df5
silviooosilva <gasparsilvio7@gmail.com>
2022-11-07 16:23:51 +0100

    Novos helpers para o upload de imagens

--- _app/boot/helpers.php
+++ _app/boot/helpers.php
@@ -1,26 +1,49 @@
 <?php
 
-include_once __DIR__."/config.php";
+include_once __DIR__ . "/config.php";
 
 function is_email(string $email): bool
 {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);    
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
-function is_Password_equal(string $var1, string $var2): bool
+
+function is_password(string $password): bool
 {
-    if($var1 != $var2){
+    if ($password < CONF_PASSWD_MIN_LEN && $password > CONF_PASSWD_MAX_LEN) {
         return false;
     }
     return true;
 }
 
-function is_password(string $password): bool
+/**
+ * UPLOAD HELPERS
+ */
+
+
+function max_size_allowed($file): bool
 {
-    if($password < CONF_PASSWD_MIN_LEN && $password > CONF_PASSWD_MAX_LEN){
-            return false;
+    /**
+     * Tamanho máximo permitido: 3MB
+     */
+
+    $convertByteToKB = 0.001 * $file;
+    if ($convertByteToKB > 3072) {
+        return false;
     }
     return true;
 }
 
+function rename_image($file): string
+{
+    return time() . mb_strstr($file, '.');
+}
 
+function uploader($origin, $destination): bool
+{
+
+    if (move_uploaded_file($origin, $destination)) {
+        return true;
+    }
+    return false;
+}
