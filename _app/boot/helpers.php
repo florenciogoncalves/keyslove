89ab00680a77b08f7ec2781c6a454c74dcceb5c5
silviooosilva <gasparsilvio7@gmail.com>
2022-11-16 00:08:39 +0100

    New Helper

--- _app/boot/helpers.php
+++ _app/boot/helpers.php
@@ -74,3 +74,17 @@ function dispatch(string $target)
 
 
 }
+
+function str_limit_words(string $string, int $limit, string $pointer = "..."): string
+{
+    $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
+    $arrWords = explode(" ", $string);
+    $numWords = count($arrWords);
+
+    if ($numWords < $limit) {
+        return $string;
+    }
+
+    $words = implode(" ", array_slice($arrWords, 0, $limit));
+    return "{$words}{$pointer}";
+}
