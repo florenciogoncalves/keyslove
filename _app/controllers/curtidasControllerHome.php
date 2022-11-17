85f95c8f3213257b9523b1b03d857445aac6370f
silviooosilva <gasparsilvio7@gmail.com>
2022-11-17 08:02:20 +0100

    LikesController Home

--- _app/controllers/curtidasControllerHome.php
+++ _app/controllers/curtidasControllerHome.php
@@ -1,16 +1,29 @@
 <?php
 require __DIR__ . "./../models/profileModel.php";
 
+$normalReacts = [];
+$otherReactions = [];
+$reacts = [
+    'retry',
+    'maybe',
+    'favorite',
+    'like',
+    'deslike'
+];
+
 
 if (isset($_POST['like'])) {
 
     $curtido = $_POST['like'];
     $action = 'like';
+    $normalReacts[] = $action;
 
 }
 if (isset($_POST['deslike'])) {
     $curtido = $_POST['deslike'];
     $action = 'deslike';
+    $normalReacts[] = $action;
+
 }
 
 if (isset($_POST['retry'])) {
@@ -18,39 +31,39 @@ if (isset($_POST['retry'])) {
     $curtido = $_POST['retry'];
     $react = 'retry';
 
+    $otherReactions[] = $react;
+
 }
 if (isset($_POST['maybe'])) {
     $curtido = $_POST['maybe'];
     $react = 'maybe';
+
+    $otherReactions[] = $react;
+
 }
 if (isset($_POST['favorite'])) {
     $curtido = $_POST['favorite'];
     $react = 'favorite';
+
+    $otherReactions[] = $react;
+
 }
 if (isset($_SESSION['username'])) {
     $user = $_SESSION['username'];
 }
 
-$otherReacts = [
-    'maybe',
-    'favorite',
-    'retry'
-];
 
 
-if ($react == 'retry' || $react == 'maybe' || $react == 'favorite') {
 
-    ((new profileModel))->addNewReacts($user, $curtido, $react);
+
+foreach ($reacts as $item) {
+    if (in_array($item, $otherReactions)) {
+        ((new profileModel))->addNewReacts($user, $curtido, $item);
+    }
+
+    if (in_array($item, $normalReacts)) {
+        ((new profileModel))->reactAction($user, $curtido, $item);
+
+    }
 
 }
-
-((new profileModel))->addNewReacts($user, $curtido, $react);
-
-// // if ($action == 'like' || $action == 'deslike') {
-
-//     ((new profileModel))->reactAction($user, $curtido, $action);
-
-// // }
-
-
-var_dump($_REQUEST);
