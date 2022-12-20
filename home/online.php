45b2948e3a8d8f876f907cd82451170deb0107a2
silviooosilva <gasparsilvio7@gmail.com>
2022-12-20 14:26:39 +0100

    Commit

--- home/online.php
+++ home/online.php
@@ -19,8 +19,8 @@ if (!$_SESSION['username']) {
   <link rel="shortcut icon" href="./../images/favicon.svg" type="image/x-icon" />
   <link rel="stylesheet" href="./../style/style.css" />
   <link rel="stylesheet" href="./../style/style-responsivo.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js" integrity="sha512-tWHlutFnuG0C6nQRlpvrEhE4QpkG1nn2MOUMWmUeRePl4e3Aki0VB6W1v3oLjFtd0hVOtRQ9PHpSfN6u6/QXkQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-  <script src="./../script/ajax.js"></script>
 
 </head>
 
