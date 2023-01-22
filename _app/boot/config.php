d57b2a37e687794174dc5cdd09d7f5fd9e6aa3fa
silviooosilva <gasparsilvio7@gmail.com>
2023-01-22 23:24:41 +0100

    Mail settings

--- _app/boot/config.php
+++ _app/boot/config.php
@@ -16,21 +16,20 @@ define("CONF_DB_NAME", "keyslov_bd");
  */
 define("CONF_PASSWD_MIN_LEN", 8);
 define("CONF_PASSWD_MAX_LEN", 40);
-// define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
-// define("CONF_PASSWD_OPTION", ["cost" => 10]);
+
 
 
 /**
  * MAIL
  */
-define("CONF_MAIL_HOST", "smtp.mailtrap.io");
-define("CONF_MAIL_PORT", "2525");
-define("CONF_MAIL_USER", "cc9068549ff8b0");
-define("CONF_MAIL_PASS", "a948e504e232a0");
-define("CONF_MAIL_SENDER", ["name" => "Equipe Keyslove", "address" => "keyslove2022teste@gmail.com"]);
-define("CONF_MAIL_SUPPORT", "keyslove2022teste@gmail.com");
+define("CONF_MAIL_HOST", "p3plzcpnl499979.prod.phx3.secureserver.net");
+define("CONF_MAIL_PORT", "465");
+define("CONF_MAIL_USER", "noreplykeyslove@aplicativo1.store");
+define("CONF_MAIL_PASS", "*97B=23y2K14");
+define("CONF_MAIL_SENDER", ["name" => "Equipe Keyslove", "address" => "noreplykeyslove@aplicativo1.store"]);
+define("CONF_MAIL_SUPPORT", "noreplykeyslove@aplicativo1.store");
 define("CONF_MAIL_OPTION_LANG", "br");
 define("CONF_MAIL_OPTION_HTML", true);
 define("CONF_MAIL_OPTION_AUTH", true);
-define("CONF_MAIL_OPTION_SECURE", "tls");
+define("CONF_MAIL_OPTION_SECURE", "ssl");
 define("CONF_MAIL_OPTION_CHARSET", "utf-8");
